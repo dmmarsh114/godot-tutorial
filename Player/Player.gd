ae6c5cbc49d3fba77bbc9e28fdfc8f9931dd70b9
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-09-04 16:06:34 -0400

    added bat enemy

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var ROLL_SPEED = 100
+export var FRICTION = 500
 
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 enum {
 	MOVE,
@@ -17,18 +19,20 @@ var state = MOVE
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 		ROLL:
-			roll_state(delta)
+			roll_state()
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 	
 
 func move_state(delta):
@@ -38,28 +42,44 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		# set the attack blend state after movement, so player can't change direction mid-attack
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)		
 		animationState.travel("Run")
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
-	velocity = move_and_slide(velocity)
+	
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+		
+	if Input.is_action_just_pressed("Roll"):
+		state = ROLL
 	
-func roll_state(delta):
-	pass
+func roll_state():
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
 	
-func attack_state(delta):
+func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
+func move():
+	velocity = move_and_slide(velocity)
+	
 func attack_animation_finished():
+	state = MOVE
+	
+func roll_animation_finished():
+	# reduce velocity after roll to minimize sliding
+	velocity = velocity * 0.8
 	state = MOVE
---
+++ Player/SwordHitbox.gd
@@ -0,0 +1,3 @@
+extends "res://Combat Collisions/Hitbox.gd"
+
+var knockback_vector = Vector2.ZERO
